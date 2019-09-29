8f630f78abdf97f040a282b9d92a3dad93ee0d74
madeInLagny <madeInLagny@gmail.com>
2019-09-29 17:52:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# mil-pulse-spinner
-Pulse Spinner made with litElement
+# \<mil-pulse-spinner\>
+
+'mil-pulse-spinner' is a loading spinner that exposes a pulse animation. It uses lit-element and can be used with any webcomponent.
+
+## Usage
+
+### Install from npm
+
+```sh
+npm install --save mil-pulse-spinner
+```
+
+### Import to your webcomponent
+
+```js
+import "mil-pulse-spinner";
+```
+
+### Add to html
+
+```js
+<mil-pulse-spinner id="myMilPulseSpinner"></mil-pulse-spinner>
+```
+
+### Remove element when needed
+
+When loading is complete, simply remove the element.
+
+```js
+const spinner = this.shadowRoot.getElementById("myMilPulseSpinner");
+
+targetElement.addEventListener("loaded", function() {
+  spinner.remove();
+});
+```
+
+## Styling
+
+Use customs CSS properties for styling.
+
+| CSS property | Default | 
+| -------------------- | ------- |
+| `--color1` | Black | 
+| `--color2` | White |
+| `--width` | 40px  |
+| `--height`   | 40px  |
---
+++ mil-pulse-spinner.js
@@ -0,0 +1,77 @@
+import { LitElement, html, css } from "lit-element";
+
+export class milPulseSpinner extends LitElement {
+  static get styles() {
+    return [
+      css`
+        .spinner {
+          width: var(--width, 40px);
+          height: var(--height, 40px);
+          position: relative;
+          margin: 100px auto;
+        }
+        .double-bounce1,
+        .double-bounce2 {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          opacity: 0.6;
+          position: absolute;
+          top: 0;
+          left: 0;
+
+          -webkit-animation: sk-bounce 2s infinite ease-in-out;
+          animation: sk-bounce 2s infinite ease-in-out;
+        }
+
+        .double-bounce1 {
+          background-color: var(--color1, black);
+        }
+
+        .double-bounce2 {
+          background-color: var(--color2, white);
+        }
+
+        .double-bounce2 {
+          -webkit-animation-delay: -1s;
+          animation-delay: -1s;
+        }
+
+        @-webkit-keyframes sk-bounce {
+          0%,
+          100% {
+            -webkit-transform: scale(0);
+          }
+
+          50% {
+            -webkit-transform: scale(1);
+          }
+        }
+
+        @keyframes sk-bounce {
+          0%,
+          100% {
+            transform: scale(0);
+            -webkit-transform: scale(0);
+          }
+
+          50% {
+            transform: scale(1);
+            -webkit-transform: scale(1);
+          }
+        }
+      `
+    ];
+  }
+
+  render() {
+    return html`
+      <div class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+      </div>
+    `;
+  }
+}
+
+customElements.define("mil-pulse-spinner", milPulseSpinner);
